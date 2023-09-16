4c5243ad6587c67c9d8fd692b22fc7670bbc590f
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-09-16 13:42:37 +0530

    Update index.js

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -1,36 +1,86 @@
-import Category from '../category'
-
-import Rating from '../rating'
-
 import './index.css'
 
 const FiltersGroup = props => {
-  const {categoryList, ratingList, click, reset1} = props
+  const {input1} = props
 
-  const upt = (categoryId, ratingId, event) => {
-    click(event.target.value, categoryId, ratingId)
+  const upt = event => {
+    const {input} = props
+    input(event.target.value)
+  }
+
+  const entered = event => {
+    const {enter} = props
+
+    if (event.key === 'Enter') {
+      enter()
+    }
   }
 
   const reset = () => {
-    reset1()
+    const {set1} = props
+    set1()
   }
+
+  const Category = () => {
+    const {categoryList, click} = props
+
+    return categoryList.map(each => {
+      const cat = () => click(each.categoryId)
+      return (
+        <li key={each.categoryId} onClick={cat}>
+          <p>{each.name}</p>
+        </li>
+      )
+    })
+  }
+
+  const Category1 = () => (
+    <>
+      <h1>Category</h1>
+      <ul>{Category()}</ul>
+    </>
+  )
+
+  const Rating = () => {
+    const {ratingList, rating} = props
+
+    return ratingList.map(each => {
+      const Rate = () => rating(each.ratingId)
+
+      return (
+        <li key={each.ratingId} onClick={Rate} className="li">
+          <img
+            src={each.imageUrl}
+            alt={`rating ${each.ratingId}`}
+            className="stars"
+          />
+          <p>& up</p>
+        </li>
+      )
+    })
+  }
+
+  const Rating1 = () => (
+    <>
+      <h1>Rating</h1>
+      <ul>{Rating()}</ul>
+    </>
+  )
 
   return (
     <div className="filters-group-container">
       <h1>Filters Group</h1>
-      <input type="search" placeholder="Search" onChange={upt} />
-      <ul>
-        <h1>Category</h1>
-        {categoryList.map(each => (
-          <Category cat={each} key={each.categoryId} clik={upt} />
-        ))}
-      </ul>
-      <ul>
-        <h1>Rating</h1>
-        {ratingList.map(each => (
-          <Rating rat={each} key={each.ratingId} ck={upt} />
-        ))}
-      </ul>
+      <input
+        type="search"
+        value={input1}
+        placeholder="Search"
+        onChange={upt}
+        onKeyDown={entered}
+      />
+
+      {Category1()}
+      {Rating1()}
+
       <button type="button" className="btn1" onClick={reset}>
         Clear Filters
       </button>
